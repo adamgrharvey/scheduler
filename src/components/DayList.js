0f20ac67bd34af4be9de05f64c979cc35e1f49b1
Adam <adamgrharvey@gmail.com>
2022-08-09 16:13:50 -0700

    adds daylist to the nav bar

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,6 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
 
   const days = props.days.map((day) => {
-    console.log(day);
-    console.log(props);
     return (
       <DayListItem 
         key={day.id}
